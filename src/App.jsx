38c85a1d8a0cebb6a8f71a380c5522c39772a2f3
Will Wagner <wowagner2015@yahoo.com>
2025-06-15 14:00:17 -0700

    fixed lowercase g to capital G

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import CsvGenerator from './components/Csvgenerator'
+import CsvGenerator from './components/CsvGenerator'
 
 function App() {
   const [count, setCount] = useState(0)
